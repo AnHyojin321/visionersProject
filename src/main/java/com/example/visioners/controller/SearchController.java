0dbd8c131311f01355b577df0ce5fca1aa02ac34
Anhyojin <anhj32118@gmail.com>
2024-05-02 22:36:08 +0900

    게시글 검색(제목/작성자)

--- src/main/java/com/example/visioners/controller/SearchController.java
+++ src/main/java/com/example/visioners/controller/SearchController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,18 +22,18 @@ public class SearchController {
 
     @GetMapping("/search")
     public String searchPosts(@RequestParam String searchBy, @RequestParam String keyword, Model model) {
-        List<Post> searchResults;
+        List<Post> searchResults = new ArrayList<>();  // 빈 리스트로 초기화
 
         if ("title".equals(searchBy)) {
             searchResults = postService.findByTitleContaining(keyword);
         } else if ("author".equals(searchBy)) {
             searchResults = postService.findByAuthorContaining(keyword);
         } else {
-            // 검색 조건이 잘못된 경우, 예외처리 또는 기본적으로 모든 게시물을 가져오도록 설정할 수 있습니다.
-            searchResults = postService.getAllPosts();
+            searchResults = postService.getAllPosts();  // 예외 처리: 잘못된 검색 조건
         }
 
         model.addAttribute("posts", searchResults);
-        return "search-results"; // 검색 결과를 보여줄 템플릿의 이름
+        return "board/search-results";  // 검색 결과를 보여줄 템플릿의 이름
     }
+
 }
--- src/main/java/com/example/visioners/service/PostService.java
+++ src/main/java/com/example/visioners/service/PostService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,11 +43,14 @@ public class PostService {
         // 추가적인 저장 로직이 필요한 경우 구현
     }
 
-    public List<Post> findByTitleContaining(String keyword) {
-        return null;
+    public List<Post> findByTitleContaining(String title) {
+        return postRepository.findByTitleContaining(title) != null ? postRepository.findByTitleContaining(title) :
+                new ArrayList<>();
     }
 
-    public List<Post> findByAuthorContaining(String keyword) {
-        return null;
+    public List<Post> findByAuthorContaining(String author) {
+        return postRepository.findByAuthorContaining(author) != null ? postRepository.findByAuthorContaining(author) :
+                new ArrayList<>();
     }
+
 }
